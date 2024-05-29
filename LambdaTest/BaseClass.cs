6f0a0e21548d54e15b53101fb244140aba0c14a0
Mohsin Naveed <Mohsin Naveed@DESKTOP-3GGOD3I>
2024-05-29 10:42:28 +0500

    Add project files.

---
+++ LambdaTest/BaseClass.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager.Helpers;
+
+namespace LambdaTest
+{
+    public class BaseClass : IDisposable
+    {
+        protected IWebDriver driver;
+
+        protected BaseClass() {
+            new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        public void GotoLambdaTestSite() {
+            driver.Navigate().GoToUrl("https://lambdatest.github.io/sample-todo-app/");
+        }
+
+        #region WebElements
+        public IWebElement TodoTextInput() {
+            return driver.FindElement(By.Id("sampletodotext"));
+        }
+
+        public IWebElement AddBtn() {
+            return driver.FindElement(By.Id("addbutton"));
+        }
+
+        public List<IWebElement> TodoList() { 
+            return driver.FindElement(By.ClassName("list-unstyled")).FindElements(By.TagName("li")).ToList();
+        }
+
+        #endregion
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ LambdaTest/LambdaTestGithub.cs
@@ -0,0 +1,55 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace LambdaTest
+{
+
+    /// <summary>
+    /// Testing Lambdatest sample app : https://lambdatest.github.io/sample-todo-app/
+    /// </summary>
+    public class LambdaTestGithub : BaseClass
+    {
+
+        /// <summary>
+        /// Verify that title is correct
+        /// </summary>
+        [Fact]
+        [Trait("Priority","1")]
+        [Trait("Category", "Sanity")]
+        public void TitleVerification()
+        {
+            GotoLambdaTestSite();
+            Assert.Equal("Sample page - lambdatest.com", driver.Title);
+        }
+
+        [Fact]
+        [Trait("Priority", "2")]
+        [Trait("Category", "Functional")]
+        public void AddDateToList() {
+            GotoLambdaTestSite();
+            IWebElement listItem;
+
+            DateTime bDate = new DateTime(1994, 05, 29);
+            string bDay = bDate.ToString("d");
+
+            TodoTextInput().SendKeys(bDay);
+
+            AddBtn().Click();
+
+            //Thread.Sleep(2000);
+
+            //foreach (IWebElement li in TodoList()) {
+            //    if (li.Text.Equals(bDay)){
+            //        listItem = li;
+            //        li.Click();
+            //    }
+            //}
+
+            listItem = TodoList().Last();
+            listItem.FindElement(By.TagName("input")).Click();
+
+            string check = listItem.FindElement(By.TagName("span")).GetAttribute("class");
+            Assert.Equal("done-true", check);
+        }
+    }
+}
