097ba311980e4a9a099efcb6c6fa146e0862faa4
Mohsin naveed <mohsin.14@live.com>
2024-06-05 04:04:46 +0500

    1. Added the functionality to check item 2. In progress: functionality to verify items left

--- LambdaTest/TodoMVC.cs
+++ LambdaTest/TodoMVC.cs
@@ -1,33 +1,36 @@
-﻿using OpenQA.Selenium;
+﻿using AngleSharp.Text;
+using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
 
 namespace LambdaTest
 {
     public class TodoMVC : TodoMVCBaseClass
     {
         [Theory]
-        [InlineData("Lit")]
+        //[InlineData("Lit")]
         [InlineData("React")]
-        [InlineData("React Redux")]
-        [InlineData("Vue.js")]
-        [InlineData("Preact")]
-        [InlineData("Backbone.js")]
-        [InlineData("Angular")]
-        [InlineData("Ember.js")]
-        [InlineData("KnockoutJS")]
-        [InlineData("Dojo")]
-        [InlineData("Knockback.js")]
-        [InlineData("CanJS")]
-        [InlineData("Polymer")]
-        [InlineData("Mithril")]
-        [InlineData("Marionette.js")]
+        //[InlineData("React Redux")]
+        //[InlineData("Vue.js")]
+        //[InlineData("Preact")]
+        //[InlineData("Backbone.js")]
+        //[InlineData("Angular")]
+        //[InlineData("Ember.js")]
+        //[InlineData("KnockoutJS")]
+        //[InlineData("Dojo")]
+        //[InlineData("Knockback.js")]
+        //[InlineData("CanJS")]
+        //[InlineData("Polymer")]
+        //[InlineData("Mithril")]
+        //[InlineData("Marionette.js")]
         public void VerifyItemsAreAddedInReact(string appName)
         {
             GotoTodoMVCSite();
             GoToApp(appName);
             AddTodoItem("Wash Car");
             VerifyItemAdded("Wash Car");
+            VerifyNumberOfItems(1);
+            //CheckTodoItem("Wash Car");
         }
 
-         
     }
 }
--- LambdaTest/TodoMVCBaseClass.cs
+++ LambdaTest/TodoMVCBaseClass.cs
@@ -1,6 +1,5 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
@@ -14,26 +13,26 @@ namespace LambdaTest
     {
         public const int TIME_TO_WAIT_FOR_ELEMENT = 5;
         public IWebDriver driver;
-        WebDriverWait wait;
-        Actions action;
+        public WebDriverWait wait;
+        public Actions action;
 
         public TodoMVCBaseClass()
         {
 
-            new DriverManager().SetUpDriver(new FirefoxConfig(), VersionResolveStrategy.Latest);
-            driver = new FirefoxDriver();
-            
-            //ChromeOptions options = new ChromeOptions();
-            ////Resolved: HTTP request to the remote WebDriver server for URL http://localhost:52847/….. timed out after 60 seconds
-            //options.AddArgument("--start-maximized"); // open Browser in maximized mode
-            ////options.AddArguments("disable-infobars"); // disabling infobars
-            ////options.AddArguments("--disable-extensions"); // disabling extensions
-            ////options.AddArguments("--disable-gpu"); // applicable to windows os only
-            ////options.AddArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-            //options.AddArgument("--no-sandbox"); // Bypass OS security model
-            //new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
-            //driver = new ChromeDriver(options);
-            driver.Manage().Window.Maximize();
+            //new DriverManager().SetUpDriver(new FirefoxConfig(), VersionResolveStrategy.Latest);
+            //driver = new FirefoxDriver();
+
+            ChromeOptions options = new ChromeOptions();
+            //Resolved: HTTP request to the remote WebDriver server for URL http://localhost:52847/….. timed out after 60 seconds
+            options.AddArgument("--start-maximized"); // open Browser in maximized mode
+            //options.AddArguments("disable-infobars"); // disabling infobars
+            //options.AddArguments("--disable-extensions"); // disabling extensions
+            //options.AddArguments("--disable-gpu"); // applicable to windows os only
+            //options.AddArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+            options.AddArgument("--no-sandbox"); // Bypass OS security model
+            new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
+            driver = new ChromeDriver(options);
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(180);
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(TIME_TO_WAIT_FOR_ELEMENT));
             action = new Actions(driver);
         }
@@ -53,11 +52,6 @@ namespace LambdaTest
             GetAppLink(v).Click();
         }
 
-        private IWebElement GetAppLink(string v)
-        {
-            return wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath($"//span[text()='{v}']")));
-        }
-
         public void AddTodoItem(string v)
         {
             action.Click(TodoInput()).SendKeys(v).SendKeys(Keys.Enter).Perform();
@@ -65,19 +59,54 @@ namespace LambdaTest
 
         public void VerifyItemAdded(string v)
         {
-            string todoItem = "";
+            string todoItem;
             try
             {
                 todoItem = wait.Until(ExpectedConditions.ElementExists(By.XPath($"//label[text()='{v}']"))).Text;
             }
-            catch (Exception NoSuchElementException)
+            catch (Exception ex)
             {
-                todoItem = GetShadowedTodoListItem(v);
+                todoItem = GetShadowedTodoListItem(v).Text;
             }
             Assert.Equal(v, todoItem);
         }
 
+        public void CheckTodoItem(string v)
+        {
+            try
+            {
+                wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath($"//label[text()='{v}']/preceding-sibling::input]"))).Click();
+            }
+            catch (Exception ex)
+            {
+                GetShadowedTodoListItemCheckbox(v).Click();
+            }
+        }
+
+        public void VerifyNumberOfItems(int v)
+        {
+            int actualItemsLeft = GetTodoItemLeftCount(GetTodoItemLeftElement());
+            Assert.Equal(v, actualItemsLeft);
+        }
+
+        public int GetTodoItemLeftCount(IWebElement itemLeftElem)
+        {
+            string itemLeft = itemLeftElem.Text.ToString().Split(" ")[0];
+            return Int32.Parse(itemLeft);
+        }
+
+        /// <summary>
+        /// //// To find WebElements
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+
         #region Webelement
+
+        private IWebElement GetAppLink(string v)
+        {
+            return wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath($"//span[text()='{v}']")));
+        }
 
         public IWebElement TodoInput()
         {
@@ -108,16 +137,56 @@ namespace LambdaTest
             return GetShadowedTodoAppRoot().FindElement(By.CssSelector("todo-list")).GetShadowRoot();
         }
 
-        public String GetShadowedTodoListItem(String v)
+        public ISearchContext GetShadowedTodoFooterRoot()
         {
-            String todoItem = "";
-            List<IWebElement> listItems = GetShadowedTodoListRoot().FindElements(By.CssSelector("todo-item")).ToList();
-            IWebElement i = GetShadowedTodoListRoot().FindElement(By.CssSelector("todo-item"));
-            foreach (IWebElement item in listItems)
-                if (item.GetShadowRoot().FindElement(By.CssSelector("label")).Text.Equals(v))
-                    todoItem = v;
+            return GetShadowedTodoAppRoot().FindElement(By.CssSelector("todo-footer")).GetShadowRoot();
+        }
 
-            return todoItem;
+        public List<IWebElement> GetShadowedTodoListItems()
+        {
+            return GetShadowedTodoListRoot().FindElements(By.CssSelector("todo-item")).ToList();
+        }
+
+        public IWebElement GetShadowedTodoListItem(String v)
+        {
+            IWebElement todoItem;
+            List<IWebElement> listItems = GetShadowedTodoListItems();
+            foreach (IWebElement item in listItems)
+            {
+                todoItem = item.GetShadowRoot().FindElement(By.CssSelector("label"));
+                if (todoItem.Text.Equals(v))
+                {
+                    return todoItem;
+                }
+            }
+            throw new NoSuchElementException("List item not found");
+        }
+
+        public IWebElement GetShadowedTodoListItemCheckbox(String v)
+        {
+            IWebElement todoItem;
+            List<IWebElement> listItems = GetShadowedTodoListItems();
+            foreach (IWebElement item in listItems)
+            {
+                todoItem = item.GetShadowRoot().FindElement(By.CssSelector("label"));
+                if (todoItem.Text.Equals(v))
+                {
+                    return item.GetShadowRoot().FindElement(By.CssSelector("input"));
+                }
+            }
+            throw new NoSuchElementException("List item not found");
+        }
+
+        public IWebElement GetTodoItemLeftElement()
+        {
+            try
+            {
+                return wait.Until(ExpectedConditions.ElementExists(By.CssSelector("span[class='todo-count']")));
+            }
+            catch (Exception ex)
+            {
+                return GetShadowedTodoFooterRoot().FindElement(By.CssSelector("span[class='todo-count']"));
+            }
         }
 
 
